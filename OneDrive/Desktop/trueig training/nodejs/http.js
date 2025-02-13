02c2778052fece61b462f136a77bcc3bf9a98abf
namantrueig <nsharma@trueigtech.com>
2025-02-13 10:50:41 +0530

    Delete OneDrive/Desktop/trueig training directory

--- OneDrive/Desktop/trueig training/nodejs/http.js
+++
@@ -1,9 +0,0 @@
-const http= require('http');
-
-const server = http.createServer((req,res)=>{
-    
-    fs.appendfile("log.txt","hello",(err,res)=>{
-        console.log("log created")
-    })
-    res.end("hello from server");
-}).listen(5000,()=>{console.log("server started")});
